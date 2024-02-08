43218597b1033862405eed593df94ab5a69a374d
fuad <fbader@kayanhr.com>
2024-02-08 07:37:04 +0000

    Class lib creation

---
+++ Cypher/Cypher.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using StackExchange.Redis;
+using System.IO;
+
+namespace Cypher;
+
+public class Cypher
+{
+    public void RedisConn()
+    {
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"); 
+        IDatabase db = redis.GetDatabase();
+        byte[] imageData = File.ReadAllBytes("../../buckets/michaelsoft/emp-img/1(1).png");
+        db.StringSet("image_key", imageData);
+        Console.WriteLine("Image uploaded to Redis.");
+    }
+}
