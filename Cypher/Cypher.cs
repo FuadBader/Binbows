58e8af4163441e600539fa8a0b4a449eb4887031
Fuad Bader <fbader@kayanhr.com>
2024-02-08 12:47:47 +0300

    added gitignore

--- Cypher/Cypher.cs
+++ Cypher/Cypher.cs
@@ -8,10 +8,10 @@ public class Cypher
 {
     public void RedisConn()
     {
-        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost"); 
+        ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost");
         IDatabase db = redis.GetDatabase();
         byte[] imageData = File.ReadAllBytes("../../buckets/michaelsoft/emp-img/1(1).png");
         db.StringSet("image_key", imageData);
-        Console.WriteLine("Image uploaded to Redis.");
+        // Console.WriteLine("Image uploaded to Redis.");
     }
 }
